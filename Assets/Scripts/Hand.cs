60581a483dd12eb914200abccd7d1c5f0d164cbe
HSW42544869 <73380363+HSW42544869@users.noreply.github.com>
2022-03-01 11:55:56 +0800

    2022.3.1.11.55

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -3,27 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
 {
+    Animator animator;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurren;
+    private float triggerCurren;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AnimateHand();
     }
 
     internal void setGrip(float v)
     {
-        throw new NotImplementedException();
+        gripTarget = v;
     }
 
     internal void SetTrigger(float v)
     {
-        throw new NotImplementedException();
+        triggerTarget = v;
+    }
+    void AnimateHand()
+    {
+        if (gripCurren != gripTarget)
+        {
+            gripCurren = Mathf.MoveTowards(gripCurren, gripTarget, Time.deltaTime);
+        }
     }
 }
